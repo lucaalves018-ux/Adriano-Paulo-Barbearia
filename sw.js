109208e72b661f11092ad26a8ed0ffba4db28b73
lucaalves018-ux <lucaalves018@gmail.com>
2026-09-10 07:44:03 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'adriano-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // Apenas deixa passar, sem cache
+});
